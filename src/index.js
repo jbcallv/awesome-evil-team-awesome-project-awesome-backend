bb36926f812db8edf256ad3bab3591d481fa0c4a
Joseph Call <jbcallv@gmail.com>
2023-04-01 06:07:39 -0400

    auth code request in place

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import querystring from "querystring";
+import cors from "cors";
 import utils from "./utils.js";
 
 dotenv.config()
 
 const app = express()
+app.use(cors());
 
 /**
  * user-defined api endpoints here
@@ -13,25 +15,23 @@ const app = express()
 app.get("/login", (req, res) => {
   var state = utils.generateRandomString(16);
   var scope = 'user-read-private user-read-email';
+  console.log(req)
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
+  res.send('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
       scope: scope,
-      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+      redirect_uri: req.query.redirect_uri,
       state: state
     }));
 })
 
-
 app.get('/', (req, res) => {
-  res.send("Init!")
+  res.json({success: true})
 })
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Mixify Me on localhost:${PORT}`)
 })
-
-
