40261cf29b6ace13e5f745cdc5e49edea7ff1fbb
Joseph Call <jbcallv@gmail.com>
2023-04-01 06:49:56 -0400

    spotify access token generation complete

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import querystring from "querystring";
 import cors from "cors";
+import axios from "axios";
 import utils from "./utils.js";
 
 dotenv.config()
@@ -13,9 +14,8 @@ app.use(cors());
  * user-defined api endpoints here
  */
 app.get("/login", (req, res) => {
-  var state = utils.generateRandomString(16);
-  var scope = 'user-read-private user-read-email';
-  console.log(req)
+  let state = utils.generateRandomString(16);
+  let scope = 'user-read-private user-read-email';
 
   res.send('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
@@ -25,6 +25,31 @@ app.get("/login", (req, res) => {
       redirect_uri: req.query.redirect_uri,
       state: state
     }));
+})
+
+app.get("/access-token", (req, res) => {
+  let code = req.query.code;
+  let redirect_uri = req.query.redirect_uri;
+
+  let requestConfig = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Basic " + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
+    }
+  }
+
+  let body = {
+    code: code,
+    redirect_uri: redirect_uri,
+    grant_type: "authorization_code"
+  }
+
+  axios.post("https://accounts.spotify.com/api/token", body, requestConfig)
+  .then((result) => {
+    res.json({success: true, access_token: result.data.access_token});
+  }).catch((error) => {
+    res.json({success: false, message: "Invalid auth code"})
+  })
 })
 
 app.get('/', (req, res) => {
